e76b00c1c8a8e1611da7fb580038ad6316d88adf
Kamesh330 <189806260+kamesh330@users.noreply.github.com>
2026-02-23 20:42:58 +0530

    final theme change correcting the error

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "development" ? "/" : "/Hospital-management-system/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
